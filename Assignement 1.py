9837a011226a866983ab96ea62e4d690af0d8a40
itspiyushpareek <piyushpareek020@gmail.com>
2025-07-27 17:16:37 +0530

    Add files via upload

---
+++ Assignement 1.py
@@ -0,0 +1,15 @@
+#Task 1
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+print("Addition: ", (a+b))
+print("Multiplication: ", (a*b))
+print("Subtraction: ", (a-b))
+print("Division: ", (a/b))
+
+# Task 2
+
+first = input("Enter Your First Name: ")
+second = input("Enter Your Second Name: ")
+conc = first+" "+second+"!"
+print("Hello, ",conc,"Welcome to the python program")
